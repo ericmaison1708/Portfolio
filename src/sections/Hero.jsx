b1c3ebeadd9aad8ad9fc3dadf6e8deed60d9f53f
ericmaison1708 <son.dm216879@sis.hust.edu.vn>
2025-07-24 00:27:46 +0700

    hide background

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
       className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space"
     >
       <HeroText />
-      <ParallaxBackGround />
+      {/* <ParallaxBackGround /> */}
       <figure
         className="absolute inset-0"
         style={{ width: "100vw", height: "100vh" }}
